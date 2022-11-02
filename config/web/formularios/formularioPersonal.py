d269409cc655b28a95261f0c2d9ccbdd3f4c1524
mfranco <mfranco@euno.com.co>
2022-11-02 17:46:05 -0500

    NTJ13- se termina de crear el formulario para empleados

---
+++ config/web/formularios/formularioPersonal.py
@@ -0,0 +1,46 @@
+from django import forms
+
+
+class FormularioPersonal(forms.Form):
+
+    CARGO=(
+        (1, 'Chef'),
+        (2, 'Administrador'),
+        (3, 'Mesero'),
+        (4, 'subchef')
+    )
+
+    nombres=forms.CharField(
+        required=True,
+        max_length=25,
+        label="Nombres Del Empleado",
+        widget=forms.TextInput(attrs={'class':'form-control mb-3'})
+    )
+    apellidos=forms.CharField(
+        required=True,
+        max_length=25,
+        label="Apellidos Del Empleado",
+        widget=forms.TextInput(attrs={'class':'form-control mb-3'})
+    )
+    foto=forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={'class':'form-control mb-3'})
+    )
+    cargo=forms.ChoiceField(
+        required=True,
+        label="Cargo Del Empleado",
+        widget=forms.Select(attrs={'class':'form-select  mb-3'}),
+        choices=CARGO
+    )
+    salario=forms.CharField(
+        required=True,
+        max_length=10,
+        label="Salario Del Empleado",
+        widget=forms.NumberInput(attrs={'class':'form-control mb-3'})
+    )
+    contacto=forms.CharField(
+        required=True,
+        max_length=10,
+        label="Salario Del Empleado",
+        widget=forms.NumberInput(attrs={'class':'form-control mb-3'})
+    )
--- config/web/views.py
+++ config/web/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from web.formularios.formularioPlatos import FormularioPlatos
+
+from web.formularios.formularioPersonal import FormularioPersonal
+
 # Create your views here.
 
 #Las vistas en django son los CONTROLADORES
@@ -7,3 +11,26 @@ from django.shortcuts import render
 
 def Home(request):
     return render(request,'index.html')
+
+def Platos(request):
+
+    #Esta vista va a utilizar un formulario de django 
+    #DEBO CREAR ENTONCES UN OBJETO DE LA CLASE FormularioPlatos()
+    formulario=FormularioPlatos()
+
+    #CREAMOS UN DICCIONARIO PARA ENVIAR EL FORMULARIO AL HTML(TEMPLATE)
+    data={
+        'formulario':formulario
+    }
+
+    return render(request,'menuplatos.html', data)
+
+def Empleados(request):
+
+    formularioPersonal=FormularioPersonal()
+
+    data={
+        'formularioPersonal':formularioPersonal
+    }
+
+    return render(request,'registroempleados.html', data)
